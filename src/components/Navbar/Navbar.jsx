9e9eed0ceac0b733fe58181b4252a55da9f6198f
Muhammad Sameer <sameerict03@gmail.com>
2022-08-24 00:31:26 +0200

    Added the testimonial section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,7 +32,14 @@ const Navbar = () => {
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {["home", "about", "work", "skills", "contact"].map((item) => (
+              {[
+                "home",
+                "about",
+                "work",
+                "skills",
+                "testimonials",
+                "contact",
+              ].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
@@ -48,5 +55,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-// "testimonials",
